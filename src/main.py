0a7b33ad5438e70c4e5114a8f09b7a9828e9ea29
BrunoFNRodrigues <bf17669@gmail.com>
2023-08-19 23:42:38 -0300

    new main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
 
+# loader functions that you programmed!
+from model import load_model, load_encoder
+
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
+    """
+    Route to check that API is alive!
+    """
     return "Model API is alive!"
+
+
+@app.post("/predict")
+async def predict():
+    """
+    Route to make predictions!
+    """
+    # Load the models
+    ohe = load_encoder()
+    model = load_model()
+
+    return {"prediction": "I can almost make predictions!"}
